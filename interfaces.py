75f68e0340d8542ff3406cf0f46ed28a26638b0e
Miguel Brondani <brondani.miguel@gmail.com>
2024-08-28 12:46:57 -0300

    interfaceMenu, escrita de dados do configs.json

--- interfaces.py
+++ interfaces.py
@@ -1,12 +1,16 @@
 import datetime
 
-def interfaceCredenciaç():
-        id = input()
-        senha = input()
+def interfaceCredenciais():
+    id = input("ID: ")
+    senha = input("Senha: ")
+    return {"id": id, "senha": senha}
 
-        
 
 def datasComecoFinal():
     dataComeco = input("Data de comeco (yyyy-mm-dd)")
     dataFinal = input("Data final (yyyy-mm-dd, dia nao incluido)")
     return [dataComeco, dataFinal]
+
+def interfaceMenu():
+    texto = "\n0. Sair\n1. Atualizar credenciais\n2. "
+    print(texto)
--- main.py
+++ main.py
@@ -1,20 +1,43 @@
+import json
+import os
+from interfaces import *
 
-def criaConfigs():
-    path = 'configs.json'
-    existeArq =os.path.isfile(path)
+
+def criaConfigs(pathConfigs):
+    existeArq =os.path.isfile(pathConfigs)
     if(not existeArq):
-        conteudoJson = '{"Credenciais":{"Ana":""}}'
+        conteudoJson = '{"Credenciais":{"Ana":{"Identificador":"", "Senha":""}}}'
 
-        arq = open(path, 'w')
+        arq = open(pathConfigs, 'w')
         arq.write(conteudoJson)
 
         arq.close()
 
+def atualizaCredenciaisAna(pathConfigs):
+    novosDadosDict = interfaceCredenciais()
+    
+    arq = open(pathConfigs, 'r+')
+     
+    dataArqStr = arq.read()
+    dataJson = json.loads(dataArqStr)
+    
+    dataJson.update({"Credenciais":{"Ana":{"Identificador":novosDadosDict['id'], "Senha":novosDadosDict['senha']}}})
+
+    #arq.read() faz o ponteiro ir para o final do arquivo, e sendo necessário voltar pro começo para sobre-escrever as modificações
+    arq.seek(0)
+
+    arq.write(json.dumps(dataJson))
+
+    arq.close()
+
+
 def main():
-    criaConfigs()
-    entradaUser = 9
+    pathConfigs = 'configs.json'
+    criaConfigs(pathConfigs)
+    interfaceMenu()
+    entradaUser = input()
     if(entradaUser==1):
-        pass #criaCredenciais
+        atualizaCredenciaisAna(pathConfigs)
     elif():
         pass
 if __name__ == "__main__":
