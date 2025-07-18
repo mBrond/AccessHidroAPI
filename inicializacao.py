357f4a1650d63f356836e13a87599df249242563
Miguel Brondani <brondani.miguel@gmail.com>
2025-07-18 12:07:38 -0300

    inicializacao estacoes

--- inicializacao.py
+++ inicializacao.py
@@ -1,27 +1,25 @@
 import os
 
-def cria_configs(pathConfigs):
-    existeArq =os.path.isfile(pathConfigs)
-    if(not existeArq):
-        conteudoJson = '{"Credenciais":{"Ana":{"Identificador":"", "Senha":""}}}'
-
-        arq = open(pathConfigs, 'w')
-        arq.write(conteudoJson)
-
-        arq.close()
+def cria_configs(path_configs):
+    existe_arquivo = os.path.isfile(path_configs)
+    if not existe_arquivo:
+        conteudo_json = '{"Credenciais":{"Ana":{"Identificador":"", "Senha":""}}}'
+        arquivo = open(path_configs, 'w')
+        arquivo.write(conteudo_json)
+        arquivo.close()
     
-def cria_dir_resultados(pathResultados):
+def cria_dir_resultados(path_resultados):
     """Cria pasta 'resultados'"""
-    os.makedirs(pathResultados, exist_ok=True)
+    os.makedirs(path_resultados, exist_ok=True)    
 
+def cria_estacoes(path_estacoes):
+    existe_arquivo = os.path.isfile(path_estacoes)
+    if not existe_arquivo:
+        arquivo = open(path_estacoes, 'w')
+        arquivo.close()
 
-# def cria_arq_estacoes(pathEstacoes): # em utilização ?????
-#     existeArq = os.path.isfile(pathEstacoes)
-#     if(not existeArq):
-#         file = open(pathEstacoes, 'w')
-#         file.close()
-        
-
-def inicializacao_basico(pathConfigs, pathResultados):
-    cria_configs(pathConfigs)
-    cria_dir_resultados(pathResultados)
+def inicializacao_basico(path_configs, path_resultados, path_estacoes):
+    cria_configs(path_configs)
+    cria_dir_resultados(path_resultados)
+    cria_estacoes(path_estacoes)
+    
--- main.py
+++ main.py
@@ -5,13 +5,12 @@ from interfaceGrafica import Application
 from tkinter import Tk
 
 def gerenciador(pathConfigs, pathResultados, pathEstacoes, tituloJanela, qtdDownloadAsync, versao):
-    inicializacao.inicializacao_basico(pathConfigs, pathResultados)
+    inicializacao.inicializacao_basico(pathConfigs, pathResultados, pathEstacoes)
 
-    pathEstacoes = 'estacoes.txt'
-    root = Tk()
-    root.geometry("600x450")
-    app = Application(root, versao, pathConfigs, pathResultados, pathEstacoes, tituloJanela, qtdDownloadAsync)
-    root.mainloop()
+    janelaRoot = Tk()
+    janelaRoot.geometry("600x450")
+    app = Application(janelaRoot, versao, pathConfigs, pathResultados, pathEstacoes, tituloJanela, qtdDownloadAsync)
+    janelaRoot.mainloop()
 
 if __name__ == "__main__":
     pathLogs = 'logs'
@@ -20,12 +19,11 @@ if __name__ == "__main__":
     pathEstacoes = 'estacoes.txt'
     tituloJanela = 'AHAPI'
     qtdDownloadAsync = 20
-    
     versao = "1.1.0"
+    
     try:
         gerenciador(pathConfigs, pathResultados, pathEstacoes, tituloJanela, qtdDownloadAsync, versao)
-    
-    except Exception as e:
+    except Exception as erro:
         print("Houve um erro na execução do programa. Verifique o log de atividades para melhores informações")
-        cria_log(e, traceback.format_exc(), pathLogs)
+        cria_log(erro, traceback.format_exc(), pathLogs)
         input()
